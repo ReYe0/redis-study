081cca930d7530198a84c10435a397e421a2c47f
Xuyong <1299461580@qq.com>
2023-01-04 21:19:16 +0800

    分布式锁测试，postwoman偶尔会失效，把记得把接口和参数保存到md

---
+++ src/main/java/com/study/redis/util/ILock.java
@@ -0,0 +1,21 @@
+package com.study.redis.util;
+
+/**
+ * @Description: redis-study
+ * @Author: 二爷
+ * @E-mail: 1299461580@qq.com
+ * @Date: 2023/1/4 21:01
+ */
+public interface ILock {
+    /**
+     * 尝试获取锁
+     * @param timeoutSec 锁持有的超时时间，过期后自动释放
+     * @return true代表获取锁成功; false代表获取锁失败
+     */
+    boolean tryLock(long timeoutSec);
+
+    /**
+     * 释放锁
+     */
+    void unlock();
+}
